e4ad0895ffe49ccb979140e6a6623acf67a7a76c
matthew murphy <mattyhead@gmail.com>
2017-09-04 10:30:25 +0000

    first commit

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
 
-require __DIR__ '/../bootstrap.php';
+$container = require __DIR__ . '/../app/bootstrap.php';
+
+$container->getByType(Nette\Application\Application::class)
+	->run();
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,3 @@
-/* JS File */
+$(function(){
+
+});
