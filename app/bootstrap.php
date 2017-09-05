8f6ecb386004b29298aee10ef87933188874fb2e
matt murphy <mattyhead@gmail.com>
2017-09-05 13:01:27 -0400

    ...

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,17 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator;
+$configurator = new Nette\Configurator();
 
 $configurator->setDebugMode('71.175.71.185'); // enable for your remote IP
+$configurator->setDebugMode('170.115.248.20');
 $configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('America/New_York');//Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
+        ->addDirectory(__DIR__)
+        ->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
